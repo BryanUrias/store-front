4a50c12c99b5c6e1a4ceafae50ff111fecc28383
Bryan Urias <BryanUrias@Macs-iMac.attlocal.net>
2020-10-26 23:02:27 -0500

    added some functionality to MainPage and Products

--- frontend/storefront/src/App.js
+++ frontend/storefront/src/App.js
@@ -1,11 +1,33 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//components
+import Nav from "./Components/Nav/Nav";
+import MainPage from "../src/Components/MainPage/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Nav />
+
+            {/* <Checkout /> */}
+          </Route>
+          <Route path="/login">
+            <Nav />
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+            <Nav />
+            <MainPage />
+            {/* <Home /> */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
